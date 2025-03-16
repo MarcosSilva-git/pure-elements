221e0d9e26ff1f254a979b718ff87c2d4f396315
Marcos Silva <marcosjoseoliveira.mj@gmail.com>
2025-03-15 22:33:10 -0300

    feat flashcard: rotate

--- components/flashcard/main.js
+++ components/flashcard/main.js
@@ -1,3 +1,16 @@
+// const attributes = {
+//     "rotateTo": {
+//         "left": card => { 
+//             card.style.setProperty('--front-rotate-angle', '180deg');
+//             card.style.setProperty('--back-rotate-angle', '-180deg');
+//         },
+//         "right": card => { 
+//             card.style.setProperty('--front-rotate-angle', '-180deg');
+//             card.style.setProperty('--back-rotate-angle', '180deg');
+//         },
+//     }
+// }
+
 document.addEventListener("click", event => {
     const card = event.target.closest('c-flashcard')
 
@@ -7,9 +20,14 @@ document.addEventListener("click", event => {
 })
 
 function c_flashcard_toggle(card) {
+    
     if (card.hasAttribute('flipped')) {
         card.removeAttribute('flipped');
     } else {
         card.setAttribute('flipped', "");
     }
+
+    // const rotateDirection = card.getAttribute('rotateTo ') || 'left';
+
+    // attributes["rotateTo"][rotateDirection](card);
 }
